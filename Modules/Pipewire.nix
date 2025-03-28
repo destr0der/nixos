1117970fdb1b717b28da0a1381dffe49e61c3a6b
Luca <lucacrisafi@gmail.com>
2025-03-28 16:16:31 +0000

    I hate blue yeti nano

--- Modules/Pipewire.nix
+++ Modules/Pipewire.nix
@@ -8,16 +8,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-    extraConfig.pipewire."99-clock-rate" = {
-      "context.properties" = {
-        "default.clock.rate" = 44100;
-        "default.clock.allowed-rates" = [ 44100 48000 96000 192000 384000 ];
-      };
-    };
   };
 }
