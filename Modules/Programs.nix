e27c7e176ac8390e041647bd70f5f223bb57dca7
Luca <lucacrisafi@gmail.com>
2025-04-04 23:05:31 +0100

    Add Nix-LD

--- Modules/Programs.nix
+++ Modules/Programs.nix
@@ -1,8 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs = {
     firefox.enable = true;
     steam.enable = true;
+
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [ 
+        icu
+        gcc_multi
+        fontconfig
+        vulkan-headers
+        vulkan-loader
+      ] ++ (
+      with xorg; [
+        libICE
+        libSM
+        libX11
+      ]);
+    };
   };
 }
