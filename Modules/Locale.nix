6078452617dee736d03d3dd549d237c16be2e1c8
destr0der <48869550+destr0der@users.noreply.github.com>
2025-07-10 14:41:22 +0200

    locale Des typo fixed

--- Modules/Locale.nix
+++ Modules/Locale.nix
@@ -5,16 +5,16 @@
   time.timeZone = "Europe/Brussels";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF8";
+  i18n.defaultLocale = "en_GB.UTF-8";
   i18n.extraLocales = [ "nl_BE.UTF-8/UTF-8"];
   
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "nl_BE.UTF-8";
     LC_CTYPE = "nl_BE.UTF-8";
-    LC_COLLATE = "en_GB.UTF8";
+    LC_COLLATE = "en_GB.UTF-8";
     LC_IDENTIFICATION = "nl_BE.UTF-8";
     LC_MEASUREMENT = "nl_BE.UTF-8";
-    LC_MESSAGES = "en_GB.UTF8";
+    LC_MESSAGES = "en_GB.UTF-8";
     LC_MONETARY = "nl_BE.UTF-8";
     LC_NAME = "nl_BE.UTF-8";
     LC_NUMERIC = "nl_BE.UTF-8";
